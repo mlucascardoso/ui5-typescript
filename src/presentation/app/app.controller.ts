79f08738cf8c476b77b3ce94440beda5123f8241
Lucas Medeiros Cardoso <clucasmedeiros@gmail.com>
2021-08-07 16:16:42 -0300

    refactor: change components layer to presentation layer

--- src/presentation/app/app.controller.ts
+++ src/presentation/app/app.controller.ts
@@ -1,7 +1,7 @@
 import Controller from 'sap/ui/core/mvc/Controller';
 
 /**
- * @namespace ui5.typescript.controller
+ * @namespace ui5.typescript.presentation.app
  */
 export default class App extends Controller {
     public onInit(): void { }
