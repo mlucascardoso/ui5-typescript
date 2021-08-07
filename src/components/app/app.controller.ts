eae76d05a14d61b47ec37a2c993a4dd6af51fa2b
Lucas Medeiros Cardoso <clucasmedeiros@gmail.com>
2021-08-07 12:25:27 -0300

    feat: add app component

---
+++ src/components/app/app.controller.ts
@@ -0,0 +1,8 @@
+import Controller from 'sap/ui/core/mvc/Controller';
+
+/**
+ * @namespace ui5.typescript.controller
+ */
+export default class App extends Controller {
+    public onInit(): void { }
+}
