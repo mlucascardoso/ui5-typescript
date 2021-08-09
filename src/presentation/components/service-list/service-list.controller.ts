8ba70e6aa6f9a8ac2fd3f0cbf5c30ae8af26adc6
Lucas Medeiros Cardoso <clucasmedeiros@gmail.com>
2021-08-09 09:21:33 -0300

    feat: add translations

--- src/presentation/components/service-list/service-list.controller.ts
+++ src/presentation/components/service-list/service-list.controller.ts
@@ -1,9 +1,5 @@
 import Controller from 'sap/ui/core/mvc/Controller';
-import moment from 'moment';
-
 
 export default class ServiceListController extends Controller {
-    public onInit(): void {
-        alert(moment().format('MM-DD-YYYY'));
-    }
+    public onInit(): void { }
 }
